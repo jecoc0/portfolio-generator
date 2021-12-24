59e5254fb585ebaa5e0594fce0de14eaa8d60ab8
Jessica <sevorg.jess@gmail.com>
2021-12-24 14:25:34 -0700

    update to code snapshot

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
-
-const animalArray = ['dog', 'cat', 'pig'];
-
-animalArray.push('cow');
-
-const personObj = {
-  name: 'Jessica',
-  age: 30
-};
-
-personObj.age = 31;
-personObj.occupation = 'Developer';
+const profileDataArgs = process.argv.slice(2);
 
 const printProfileData = profileDataArr => {
-  profileDataArr.forEach((profileItem) => {
-  console.log(profileItem);
-  });
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
-
-//Using function expression syntax
-const addNums = (numOne, numTwo) => numOne + numTwo;
-
-
-
