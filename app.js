faa6040b1c65c32dc525f424d39b58cb9b80c933
Jessica <sevorg.jess@gmail.com>
2021-12-28 19:40:06 -0700

    add inquirer to project

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const pageHTML = generatePage(name, github);
 
-  console.log('================');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-printProfileData(profileDataArgs);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
+
