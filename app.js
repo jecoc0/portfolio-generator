abf708b4f584f7f8388f2ecba62fd4bba760c765
Jessica <sevorg.jess@gmail.com>
2021-12-28 21:02:59 -0700

    data validation

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your name!');
+        return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -41,12 +49,28 @@ Add a New Project
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: projNameInput => {
+          if (projNameInput) {
+            return true;
+          } else {
+            console.log('Please enter your project name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: projDescription => {
+          if (projDescription) {
+            return true;
+          } else {
+            console.log('Please enter your project description!')
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -58,7 +82,14 @@ Add a New Project
         type: 'input',
         name: 'link',
         message: 'Enter the GitHub link to your project. (Required)',
-        default: false
+        validate: projectLink => {
+          if (projectLink) {
+            return true;
+          } else {
+            console.log('Please enter the link to your project in GitHub!')
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
